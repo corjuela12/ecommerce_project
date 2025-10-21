89966a472687b8dbb9566e9fe095a932fa0f4cf1
corjuela12 <mitchellorjuela@gmail.com>
2025-10-21 11:05:56 -0500

    feat: display services on the home page

--- home/views.py
+++ home/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import request
+from tienda.models import Servicio
 
 def home(request):
-    return render(request, "home/home.html",{}) 
+    # devolver todos los servicios disponibles
+    servicios_disponible = Servicio.objects.all().filter(esta_disponible=True)#Obtener todos los servicios que están marcados como disponibles en la base de datos
+    
+    # Enviar la variable al template
+    return render(request, "home/home.html",{ # render funcion con tres argumentos request, ruta template, diccionario(quí envías datos desde la vista hacia el template.) 
+        'servicios_disponible': servicios_disponible # clave = variable html / valor= variable python
+    }) 
